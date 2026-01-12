48b8997e99a6b3c7bd88b1f131af24e677373859
xorea24 <joshuaregalado245@gmail.com>
2026-01-12 11:29:37 +0800

    first commit

--- routes/web.php
+++ routes/web.php
@@ -1,30 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-<<<<<<< HEAD
-=======
-use App\Http\Controllers\StudentController;
->>>>>>> f1e3744 (Initial commit - Student CRUD with update and delete)
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+use App\Http\Controllers\StudentController; // Ensure this line exists!
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-<<<<<<< HEAD
-use App\Http\Controllers\StudentController;
-
+// This one line handles View, Create, Update, and Delete
 Route::resource('students', StudentController::class);
-=======
-Route::resource('students', StudentController::class);
->>>>>>> f1e3744 (Initial commit - Student CRUD with update and delete)
