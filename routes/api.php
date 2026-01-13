940755df476713eb75f1495bd08b9f104e09923d
xorea24 <joshuaregalado245@gmail.com>
2026-01-13 09:30:32 +0800

    fixed api conflicts and installed sanctum

--- routes/api.php
+++ routes/api.php
@@ -3,25 +3,8 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-<<<<<<< HEAD
 
-Route::controller(SampleController::class)
-->group(function () {
-    Route::get('/samples', 'index');
-});
-=======
->>>>>>> f1e3744 (Initial commit - Student CRUD with update and delete)
+// Add your other API routes here without the conflict markers
