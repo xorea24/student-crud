479a29a9b6211937559c55bc82385bd8f8d5b736
xorea24 <joshuaregalado245@gmail.com>
2026-01-19 12:58:47 +0800

    new

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -61,6 +61,6 @@ class AuthController extends Controller
         $request->session()->invalidate();
         $request->session()->regenerateToken();
 
-        return redirect('/login');
+        return redirect('/');
     }
 }
